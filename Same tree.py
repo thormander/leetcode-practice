15b10afd37469600eae9941add11512e3134dd32
Thor <71967190+thormander@users.noreply.github.com>
2023-09-19 11:18:20 -0500

    Create Same tree.py

---
+++ Same tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        # base case
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        
+        # nodes not equal
+        if p.val != q.val:
+            return False
+        
+        result = self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+        return result
+
+        '''
+        recursion -- DFS; compare each node values
+
+        handle the base cases
+        '''
+        
