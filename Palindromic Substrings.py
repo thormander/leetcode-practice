0e3ac658ce410f5bec6c5846fee417e8c5653820
Thor <71967190+thormander@users.noreply.github.com>
2023-09-29 17:07:59 -0500

    Update Palindromic Substrings.py

--- Palindromic Substrings.py
+++ Palindromic Substrings.py
@@ -2,29 +2,28 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         result = 0 # hold our paindrome substrings count
         
+        def isPalindrome(left,right):
+            result = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                # we have a valid palindrome
+                result += 1
+
+                left -= 1
+                right += 1 
+            return result             
+
         for i in range(len(s)):
             
             # odd sized
             left = i
             right = i
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                # we have a valid palindrome
-                result += 1
-
-                left -= 1
-                right += 1
+            result += isPalindrome(left,right)
             
             # even sized
             left = i
             right = i + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                # we have a valid palindrome
-                result += 1
-
-                left -= 1
-                right += 1
-                
-            
+            result += isPalindrome(left,right)
+               
         return result
 
         '''
