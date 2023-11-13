1ff95f2897c16f5340507194a59ca14614d29d25
Thor <71967190+thormander@users.noreply.github.com>
2023-11-13 15:22:20 -0600

    Update 347.py

--- 347.py
+++ 347.py
@@ -1,37 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
-        frequencies = [[] for i in range(len(nums)+1)]
+        hashmap = {} # num : occurance
+        
+        # set up our bucket sort
+        bucket = []
+        for i in range(len(nums)+1):
+            bucket.append([])
+        
+
+        # count occurances
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        
+        # add values to our bucket
+        for key, value in hashmap.items():
+            bucket[value].append(key)
+        
         result = []
 
-        for num in nums:
-            hashmap[num] = hashmap.get(num,0) + 1 # incrementing
-        for key,value in hashmap.items():
-            frequencies[value].append(key)
-
-        for i in range(len(frequencies)-1,0,-1):
-            for num in frequencies[i]:
+        for i in range(len(bucket)-1,0,-1):
+            for num in bucket[i]:
                 result.append(num)
-                
-                if len(result) == k:
-                    return result
-
-
-
+            
+            if len(result) == k:
+                return result
+            
+            
+        
 
         '''
-        hashmap: key is numver --> value is occurences
+        map for counts
 
-        declare an array to the length of nums (index will correspond to occurences)
-        - +1 because 0 occurences or index doesnt count
-
-        loop for finding occurences
-
-        loop for adding the hashmap of occurences to array
-
-        loop in reverse to find the max occurences:
-            loop againg to access the sublist
-                add the first encounter to a result array
-
-                 check if we hit 'k' when length of result arrary is equal 
+        bucket sort -> where index represents occurances and value is the actual number
         '''
