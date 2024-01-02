4a054aeb4fc5ef1103398c3cf2c9300e951acf81
Thor <71967190+thormander@users.noreply.github.com>
2024-01-02 15:53:32 -0600

    Update container with most water.py

--- container with most water.py
+++ container with most water.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        answer = 0
 
         left = 0
         right = len(height) - 1
 
-        while left < right:
-            currentArea = (right-left) * min(height[right],height[left])
+        result = 0
 
-            answer = max(currentArea,answer)
+        while left < right:
+            # current area
+            area = (right - left) * min(height[left],height[right])
+            result = max(area,result)
 
             if height[left] < height[right]:
                 left += 1
@@ -16,20 +17,17 @@ class Solution:
                 right -= 1
             else:
                 left += 1
-
-        return answer
-
-
-        '''
-        2 pointers, left and right on both ends
-        maxArea variable
-
-        while l<r:
-            currentArea = (r-l) * min(of their heights)
-            increment left if height[left] smaller than height[right]
-            decrement right if its height is smaller 
-            if equal, just increment one or the other
-
-        return maxArea
-        '''
         
+        return result
+
+        
+
+        '''
+        area = (right index - left index) * min(height Left,height Right)
+
+        2 pointers = start at ends where maximize length
+            - how do we move our pointers?
+            - move the pointer whose height is less
+
+        keep a running variable for max height
+        '''
