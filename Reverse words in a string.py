a0302d8939109a9c9d56fd94a8b0b920570baa43
Thor <71967190+thormander@users.noreply.github.com>
2024-01-08 16:20:03 -0600

    Update Reverse words in a string.py

--- Reverse words in a string.py
+++ Reverse words in a string.py
@@ -1,13 +1,13 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
-        words = s.split() # get words by themselves
+
+        words = s.split()
 
         return " ".join(reversed(words))
-
+        
 
         '''
-        need to split string to individual words -> list
+        Extract the words sepearted by a space into an array
 
-        reverse the list, then .join it on a " "
+        reverse our list, and join on " "
         '''
