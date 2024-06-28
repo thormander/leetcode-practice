4ed2a7dd1b03b39656a864927298a9d841e862e8
Thor <71967190+thormander@users.noreply.github.com>
2024-06-28 10:54:05 -0500

    Update 15. 3sum.py

--- 15. 3sum.py
+++ 15. 3sum.py
@@ -1,45 +1,54 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        output = []
+        results = []
 
         for i,v in enumerate(nums):
             # check duplicate
-            if ((i > 0) and (nums[i-1] == v)):
-                continue # skip
-            
+            if (i != 0) and (v == nums[i-1]):
+                continue # skip this iteration
+
             left = i + 1
             right = len(nums) - 1
 
             while left < right:
+                # check duplicates again
+                if (left != i + 1) and (nums[left-1] == nums[left]):
+                    left += 1
+                    continue
+                elif (right != len(nums) - 1) and (nums[right + 1] == nums[right]):
+                    right -= 1
+                    continue
+
+                # check sum
                 currentSum = v + nums[left] + nums[right]
-                # base case
-                if currentSum == 0:
-                    output.append([v,nums[left],nums[right]])
+                
+                if (currentSum == 0):
+                    results.append([v,nums[left],nums[right]])
                     left += 1
                     right -= 1
-
-                    # check duplicate
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while right > left and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif currentSum > 0:
+                elif (currentSum > 0):
                     right -= 1
-                else: # we are less than 0
+                else:
                     left += 1
             
-        return output          
+        return results
+
+
             
 
-
-
+        
 '''
-incoporate 2sum with poiters (sorted)
-sort first then oslve with pointers
-_ +  _ +  _ == 0
+X _ _
 
-avoid duplicates
-    - in our iteration, if we are the same as previous, then skip
+sort it first, ( we can use 2 pointers to help us)
+
+we first iterate through nums,
+get the first number, BUT we must check for duplicates:
+    if number before is same, we need to skip.
+same goes for other 2.
+
+Break this down essentially into a 2 sum after that first step.
+
+Sorting will let us handle the duplicates
 '''
