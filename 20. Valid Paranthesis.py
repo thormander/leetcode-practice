3555f4168065511cd641bbc96bd4a1d8f15e7949
Thor <71967190+thormander@users.noreply.github.com>
2024-08-28 16:11:03 -0500

    Update 20. Valid Paranthesis.py

--- 20. Valid Paranthesis.py
+++ 20. Valid Paranthesis.py
@@ -1,35 +1,41 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        reference = {')':'(','}':'{',']':'['}
-
         stack = []
 
+        pairs = {')':'(','}':'{',']':'['}
+
         for char in s:
-            # check if open
-            if char in reference.values():
+            # check if opening par
+            if char in pairs.values():
                 stack.append(char)
-            else: # when we hit a closing
-                if (stack) and (reference[char] == stack[-1]):
-                    stack.pop()
-                else:
+                continue
+            else:
+                # closing
+                # base case
+                if not stack:
                     return False
+                else:
+                    if stack[-1] == pairs[char]:
+                        stack.pop()
+                        continue
+                    else:
+                        return False
+
         
-        if not stack:
-            return True
-        else:
+        if stack:
             return False
-
-
+        else:
+            return True
 
 '''
-know the pairs --> dictionary/map key: *closing* | value: *opening*
+stack
+ - where as we find pairs, remove from stack...
+ - if our stack is empty, that means we return true (each parantehse has a pair)
 
-iterate through
- - stack...add opens to it
- - when we hit a closing --> check top of stack and compare to value in map.
- - if equal remove from stack
- - otherwise, return False
+dictionary: what pairs with what
 
-if our stack is empty --> reutrn true 
+loop through
+ - if its opening, just add to stack and keep goin
+ - if we run into a closing, check top of our stack and see if the are a pair and remove if they are
+
 '''
-        
