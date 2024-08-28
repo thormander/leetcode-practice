a8bc7017d3b02bcfd8a48f11fba42bdb1cd4e86a
Thor <71967190+thormander@users.noreply.github.com>
2024-08-28 16:24:36 -0500

    Update and rename 155. Min Stack to 155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -3,23 +3,29 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.minstack = []
+        
+        
 
     def push(self, val: int) -> None:
-        if not self.stack:
-            self.minstack.append(val) # default to current val if its empty
-        
         self.stack.append(val)
 
-        # keep min same
-        if self.minstack[-1] <= val:
-            self.minstack.append(self.minstack[-1])
+        # min stack logic
+        if not self.minstack:
+            self.minstack.append(val)
         else:
-            self.minstack.append(val) # case where last min was greater
+            if val < self.minstack[-1]:
+                self.minstack.append(val)
+            else:
+                # keep previous value if not smaller
+                self.minstack.append(self.minstack[-1])
 
 
     def pop(self) -> None:
         self.stack.pop()
+
+        # min stack logic
         self.minstack.pop()
+        
 
     def top(self) -> int:
         return self.stack[-1]
@@ -29,14 +35,16 @@ class MinStack:
         return self.minstack[-1]
         
 
+'''
+2 stacks: 1 will be standard that we use 'stack'; 'minstack' also taht we also populate
+
+1: 1 2 3
+2: 1 1 1
+'''
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
-
 # param_4 = obj.getMin()
-
-# [0 1 2 3      ] top
-# [0 0 0 0      ]
