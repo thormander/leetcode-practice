781dbfeda37ed260c415f67051f94ca67d967d81
Thor <71967190+thormander@users.noreply.github.com>
2024-08-31 15:00:42 -0500

    Update Search in Rotated Sorted Array.py

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -1,27 +1,30 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
+    def findMin(self, nums: List[int]) -> int:
+        result = nums[0]
 
-        while l <= r:
-            mid = (l + r) // 2
+        left = 0
+        right = len(nums) - 1
 
-            if target == nums[mid]:
-                return mid
+        while left <= right:
             
-            # check if target to left of mid
-            if nums[l] <= nums[mid]:
-                if nums[l] <= target and target < nums[mid]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            # check if target to right of mid
+
+            if nums[left] < nums[right]:
+                result = min(result,nums[left])
+                break
+
+            mid = (left+right) // 2
+            result = min(result,nums[mid])
+            
+            if nums[mid] >= nums[left]:
+                left = mid + 1
             else:
-                if nums[mid] < target and target <= nums[r]:
-                    l = mid + 1
-                else:
-                    r = mid - 1                
+                right = mid - 1
+
+        return result
+
         
-        return -1
 
 
+
+        
+        
