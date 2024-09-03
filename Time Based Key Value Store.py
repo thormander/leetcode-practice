142f2a2c83f72c3360118f1222082b0861cdc1ba
Thor <71967190+thormander@users.noreply.github.com>
2024-09-03 17:49:23 -0500

    Update Time Based Key Value Store.py

--- Time Based Key Value Store.py
+++ Time Based Key Value Store.py
@@ -1,60 +1,46 @@
-class TimeMap:
+class TimeMap():
 
     def __init__(self):
-        self.map = {}
+        self.myDict = {}
     
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.map:
-            self.map[key] = []
-        self.map[key].append([value,timestamp])
-
-    def get(self, key:str, timestamp: int) -> str:
-
-        pairs =  self.map.get(key,[]) # list   
+    def set(self,key:str,value:str,timestamp:int) -> None:
+        if key not in self.myDict:
+            self.myDict[key] = []
         
-        l = 0
-        r = len(pairs) - 1
-        val = ""
-        
-        while l <= r:
-            mid = (l + r) // 2
-            if pairs[mid][1] < timestamp:
-                val = pairs[mid][0]
-                l = mid + 1
-            elif timestamp < pairs[mid][1]:
-                r = mid - 1
+        self.myDict[key].append([value,timestamp])
+    
+    def get(self,key:str,timestamp:int) -> str:
+
+        if key not in self.myDict:
+            return ""
+
+        left = 0
+        right = len(self.myDict[key]) - 1
+
+        result = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            # move right
+            if self.myDict[key][mid][1] <= timestamp:
+                left = mid + 1
+                result = self.myDict[key][mid][0]
             else:
-                val = pairs[mid][0]
-                break
+                # move left
+                right = mid - 1
         
-        return val
-
-
-
-
-
-
-    '''
-    hashmap or dictionary --> key: 'key' | value: [value,timestamp]
-
-    init:
-        need to make the map
-    set:
-        if key is not there:
-            need to make a new new item
-        else:
-            we just add on top of the existing key
-    
-    get:
-        ascending order --> binary search
-        do binary search on timestamp:
-            no values == map[mid[1]]  // check if mid value = timestamp
-             search left for closest value if
-    '''
+        return result
+            
+            
         
 
 
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+'''
+[(a,1),(a,2),(b,3)]
+'''
+
+
+
+
+        
