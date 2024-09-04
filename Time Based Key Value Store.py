39252a68f338230976a971c58fefc53ad02278bd
Thor <71967190+thormander@users.noreply.github.com>
2024-09-04 14:40:46 -0500

    Update Time Based Key Value Store.py

--- Time Based Key Value Store.py
+++ Time Based Key Value Store.py
@@ -1,46 +1,55 @@
 class TimeMap():
 
+    #initialize
     def __init__(self):
-        self.myDict = {}
-    
-    def set(self,key:str,value:str,timestamp:int) -> None:
-        if key not in self.myDict:
-            self.myDict[key] = []
-        
-        self.myDict[key].append([value,timestamp])
-    
-    def get(self,key:str,timestamp:int) -> str:
+        self.map = {}
 
-        if key not in self.myDict:
+    def set(self,key:str,value:str,timestamp:int) -> None:
+        if key not in self.map:
+            self.map[key] = []
+        
+        self.map[key].append([value,timestamp])
+        
+
+    def get(self,key:str,timestamp:int) -> str:
+        # check if key actually exists
+        if key not in self.map:
             return ""
 
         left = 0
-        right = len(self.myDict[key]) - 1
+        right = len(self.map[key]) - 1
 
         result = ""
 
         while left <= right:
             mid = (left + right) // 2
 
-            # move right
-            if self.myDict[key][mid][1] <= timestamp:
+            # when to search right
+            if self.map[key][mid][1] <= timestamp:
                 left = mid + 1
-                result = self.myDict[key][mid][0]
+                result = self.map[key][mid][0]
             else:
-                # move left
                 right = mid - 1
-        
+            
         return result
+
+
+
+
             
-            
+
+'''
+map key values
+key       value
+abc       [[val,1],[val,2],[val,3]]
+
+time stamps are sorted essentially --> binary search
+use the timestamps to determine whether we search left or right.
+'''
         
 
 
-'''
-[(a,1),(a,2),(b,3)]
-'''
-
-
-
-
-        
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
