d8bbc6c69d184eed992df539670a6b70c3ec19b4
Thor <71967190+thormander@users.noreply.github.com>
2024-10-10 12:54:43 -0500

    Update Happy Number.py

--- Happy Number.py
+++ Happy Number.py
@@ -5,17 +5,36 @@ class Solution:
         while n not in hashset:
             hashset.add(n)
 
-            calc = 0
-            while n != 0:
-                num = n % 10
-                calc += (num * num) # add the square
-                n = n // 10
-            
-            if calc == 1:
-                return True
+            n = self.sumSquares(n)
 
-            n = calc 
-        
+            if n == 1:
+                return True
         return False
 
+    def sumSquares(self, n: int) -> int:
+        total = 0
+        while n != 0:
+            num = n % 10
+            total += num * num # square
+            n = n // 10 # interger division
+        return total
 
+
+
+        
+'''
+what is happy? --> after split and adding, if we get 1 return true otherwside false
+
+logic for the split and addtion
+ - modulo to extract single number
+ - division by 10 to go the the next number place
+
+n = 2
+
+2^2 = 4
+4^2 = 16
+1^2 + 6^2 = 37
+3^2 + 7^2 = 58
+... 
+use a hashset to store these values and check if we get a repeat. --> when we can stop our loop
+'''
