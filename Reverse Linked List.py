d09ef75a5bacd466c13ad39c6f797d719b439362
Thor <71967190+thormander@users.noreply.github.com>
2024-10-23 10:57:44 -0500

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -5,15 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        left = None
+        left = None # start it at None or to the left of head
         right = head
 
         while right:
-            # start reversing
             temp = right.next
-
             right.next = left
             left = right
             right = temp
         
         return left
+
+
+
+
+'''
+given a list,
+just need to reverse
+
+use pointers to do this
+ - need a left and right
+'''
