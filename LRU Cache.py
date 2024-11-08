9ac790e68a34cc666606f0f9c060872839ab3445
Thor <71967190+thormander@users.noreply.github.com>
2024-11-08 17:30:38 -0600

    Update LRU Cache.py

--- LRU Cache.py
+++ LRU Cache.py
@@ -1,38 +1,35 @@
 class LRUCache:
-    
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = OrderedDict()
-    
-    def get(self, key:int) -> int:
-        # if key exists return value, else -1
+        self.cache = OrderedDict() # maintains insertion order
+        self.max = capacity # make it accessible
+        
+
+    def get(self, key: int) -> int:
         if key in self.cache:
-            # move accessed key to back (for ordering) where front is least used
+            # need to move it to the front (as we jsut used it)
             self.cache.move_to_end(key)
             return self.cache[key]
         else:
             return -1
 
-    def put(self, key:int, value:int) -> None:
+
+
+    def put(self, key: int, value: int) -> None:
+        
         if key in self.cache:
-            self.cache[key] = value # update if key exists
-            self.cache.move_to_end(key)
-        elif len(self.cache) < self.capacity:
-            # add to cache
+            # update (doesn't need an eviction)
             self.cache[key] = value
             self.cache.move_to_end(key)
         else:
-            # cache full
-            self.cache.popitem(last = False) # pop at front of cache
-            # add to cache
+            # may need an eviction
+            if len(self.cache) >= self.max:
+                # pop from the front
+                self.cache.popitem(last=False)
+                
             self.cache[key] = value
-            self.cache.move_to_end(key)
-
-
-            
-# USED --> basically anytime it is touched or created        
         
-
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
