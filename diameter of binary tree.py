583f44c61551368ec2a8269f19b04d794ee3c8ee
Thor <71967190+thormander@users.noreply.github.com>
2024-11-14 15:09:51 -0600

    Update diameter of binary tree.py

--- diameter of binary tree.py
+++ diameter of binary tree.py
@@ -6,25 +6,38 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        result = 0
 
-        def DFS(root):
-            nonlocal result
-            if not root:
-                return -1
+        self.diameter = 0 
 
-            left = DFS(root.left)
-            right = DFS(root.right)
-
-            result = max(result, 2 + left + right)
-
-            return 1 + max(right,left)
-        
-        DFS(root)
-        
-        return result
+        def dfs(node):
+            # base case
+            if node == None:
+                return 0
             
-        '''
-        max diameter = furthest down on the right and left of the tree
-        edges = amount nodes - 1
-        '''
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.diameter = max(self.diameter,left+right)
+
+            # return height
+            return max(left,right) + 1
+        
+        dfs(root)
+        
+        return self.diameter
+
+
+
+
+'''
+DFS --> find longest path b/t two nodes
+
+2 sub trees dig as far left and right as we can.
+but, if we do it in reverse, it will make it easier as it doesnt have to go through root
+
+diameter = 0 --> global
+
+make an other helper function to do the dfs of the tree
+    - DO NOT return diameter, we return current height!
+'''
+        
