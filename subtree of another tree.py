c66d710f1bf751b548b7885c463d3dae028fe8d2
Thor <71967190+thormander@users.noreply.github.com>
2024-11-21 15:55:51 -0600

    Update subtree of another tree.py

--- subtree of another tree.py
+++ subtree of another tree.py
@@ -6,24 +6,54 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
-        def sameTree(a,b):
-            if not a and not b:
-                return True # base case
-            elif a and b and a.val == b.val:
-                return sameTree(a.right, b.right) and sameTree(a.left, b.left)
+
+        # helper to check if same
+        def isSameTree(node,subRootNode):
+            # base cases
+            if node == None and subRootNode == None:
+                return True
+            if node == None or subRootNode == None:
+                return False
+            
+            # at this point, we know we are not workign with empty trees
+            if node.val != subRootNode.val:
+                return False
+            
+            left = isSameTree(node.left,subRootNode.left)
+            right = isSameTree(node.right,subRootNode.right)
+
+            if left == True and right == True:
+                return True
             else:
                 return False
-
-        # base case
-        if not subRoot:
-            return True
-        elif not root:
-            return False
         
-        if sameTree(root,subRoot):
+        # base cases for subTree recursion (similar to helper)
+        if subRoot == None:
             return True
+        if root == None:
+            return False
 
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        if isSameTree(root,subRoot):
+            return True
+        
+        left = self.isSubtree(root.left,subRoot)
+        right = self.isSubtree(root.right,subRoot)
 
+        return left or right
 
+        
+
+'''
+need a way to check if they are same --> some helper funciton to check if same
+
+helper is same:
+    - edge cases / base case
+        - are both nodes null? (if both, return true)
+        - is one or the other null? (if true, return false)
+        - is the value the same?
+
+recursion
+ - we can use isSubTree
+ - are the 2 roots same, if not lets move on
+    - if they are, we can call on helper to check if the trees are same
+'''
