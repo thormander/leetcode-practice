2519601ec673f540b55531ff8cef71c0b6fc3018
Thor <71967190+thormander@users.noreply.github.com>
2024-11-25 18:07:21 -0600

    Update Lowest Common Ancestor of a BST.py

--- Lowest Common Ancestor of a BST.py
+++ Lowest Common Ancestor of a BST.py
@@ -7,16 +7,37 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        current = root
 
-        while current:
-            if p.val > current.val and q.val > current.val:
-                current = current.right
-            elif p.val < current.val and q.val < current.val:
-                current = current.left
+        current_node = root
+
+        while True:
+
+            # case 1
+            if p.val > current_node.val and q.val > current_node.val:
+                current_node = current_node.right
+            # case 2
+            elif p.val < current_node.val and q.val < current_node.val:
+                current_node = current_node.left
             else:
-                return current
+                return current_node
 
-        '''
-        go down the tree, right if p,q is greater/ left if less
-        '''
+
+
+
+
+'''
+
+case where both are greater
+    - search right side
+
+case where both are less
+    - search left side
+
+LCA = any time p < current node < q
+    - basically the split
+
+anytime they both arent greater or less, we can jsut return the node we are on
+    - this still holds true for case where p or q is on our LCA
+
+we are guarenteed a result because p and q exist in the given tree
+'''
