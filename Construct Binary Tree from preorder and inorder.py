0ef1a872e8fa2f60ba49d486e9f2f4bc55bdbe9e
Thor <71967190+thormander@users.noreply.github.com>
2024-12-05 17:45:20 -0600

    Update Construct Binary Tree from preorder and inorder.py

--- Construct Binary Tree from preorder and inorder.py
+++ Construct Binary Tree from preorder and inorder.py
@@ -6,15 +6,36 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
-        # base case
         if not preorder or not inorder:
             return None
         
+        # root
         root = TreeNode(preorder[0])
-        pos = inorder.index(preorder[0])
 
-        root.left = self.buildTree(preorder[1:pos+1],inorder[:pos])
-        root.right = self.buildTree(preorder[pos+1:],inorder[pos+1:])
+        amount = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:amount+1],inorder[:amount])
+        root.right = self.buildTree(preorder[amount+1:],inorder[amount+1:])
 
         return root
+
+
+        
+
+'''
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+
+root = 3
+preorder = [3 | 9,20,15,7]
+inorder = [9 | 3 | 15,20,7]
+
+preorder = [X X 20,15,7]
+inorder = [X X 15 | 20 | 7]
+
+
+
+recursive
+    - first create root node
+
+'''
