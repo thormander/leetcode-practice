45b3c367e42389c9da84da6bf07f10cdd97f57b8
Thor <71967190+thormander@users.noreply.github.com>
2024-12-11 12:16:36 -0600

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -1,21 +1,50 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
+        subset = []
 
-        subset = [] 
+        lastIndex = len(nums) - 1
+
         def DFS(index):
-            # base case (where we hit leaf node)
-            if index >= len(nums):
-                result.append(subset.copy()) # append whatever is in the subset variable (must use copy as variable can be changed later)
+            # base case
+            if index > lastIndex:
+                result.append(subset.copy())
                 return
             
-            # case to include nums[i]
+            # left (dec. to add)
             subset.append(nums[index])
-            DFS(index + 1)
+            DFS(index+1)
 
-            # case not to
+            # right (dec. to not add)
             subset.pop()
-            DFS(index + 1)
-    
+            DFS(index+1)
+        
         DFS(0)
         return result
+
+'''
+result =[]
+subset = []
+
+decision tree problem
+[1,2,3]
+X
+|     \
+1      []
+|   \.  |. \
+1,2  1.  2.  []
+
+to do this --> DFS(index that we are on)
+
+recursion:
+    base case - if our index is out of range (more than 2 for the ex)
+    return nothing, but we should have a valid subset to add to result
+
+    go left (decision to add the number)
+
+    go right (decision to not add number)
+
+
+DFS on index 0
+return our result
+'''
