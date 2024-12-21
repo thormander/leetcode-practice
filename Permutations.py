e2aeac29c2e1bd32040ab8e959522bd6a3a606d5
Thor <71967190+thormander@users.noreply.github.com>
2024-12-21 13:40:48 +1000

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,22 +1,40 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
 
         # base case
-        if len(nums) == 1:
-            return [nums.copy()]
+        if len(nums) == 0:
+            return [[]]
         
-        for i,v in enumerate(nums):
-            n = nums.pop(0) # pop first val
+        permutations = self.permute(nums[1:]) # holds permutations
 
-            permutations = self.permute(nums) 
-            
-            # take generate permutations and add to result
-            for p in permutations:
-                # add n to end
-                p.append(n)
+        result = []
 
-            result.extend(permutations)
-            nums.append(n)
-        
+        for permuation in permutations:
+            for i in range(len(permuation)+1):
+                cur = permuation.copy()
+                cur.insert(i,nums[0])
+                result.append(cur)
+
+
         return result
+
+
+        
+
+'''
+backtracking
+
+base case:
+    we return up empty list if nums is 0
+
+recurse on istelf, and pass nums without index 0:
+    after we go down to base case, we start adding to a list
+
+
+1,2,3 
+2,3.   [[2,3],[3,2]]
+3.    [[3]]
+_.  - [[]]
+
+we only add the first index as we go up.
+'''
