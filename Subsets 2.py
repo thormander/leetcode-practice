8e6ff8647b52ea3b9a31ef47f9a4d276107b538a
Thor <71967190+thormander@users.noreply.github.com>
2024-12-23 15:11:05 +1000

    Update Subsets 2.py

--- Subsets 2.py
+++ Subsets 2.py
@@ -1,32 +1,47 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        result = []
         nums.sort()
+        result = []
 
-        # recusive function
         def backtrack(index,subset):
             # base case
             if index == len(nums):
                 result.append(subset.copy())
                 return
-
-            # recusive call including index
+            
+            # decision left
             subset.append(nums[index])
-            backtrack(index + 1, subset)
-            subset.pop()
+            backtrack(index+1,subset)
 
-            # recursive call at subsets other than index (handle skipping also)
+            # decision right
+            subset.pop() # bring back to original state
             while index + 1 < len(nums) and nums[index] == nums[index+1]:
                 index += 1
-            backtrack(index + 1, subset)
+            backtrack(index+1,subset)
         
-        backtrack(0, [])
+        backtrack(0,[])
         return result
 
 
+    
 
+'''
+backtracking
+we also need to sort the input
 
+decision tree
+    - left include the current number
+    - right don't include (BUT if next number is same, we need to skip it)
 
-        '''
-        backtrack; make sure to skip over any duplicate numbers
-        '''
+function backtrack(index, some list):
+    base case
+        - index is at the end of the list, add to our result list
+    
+    recursion left
+    
+
+    recursion right
+        - skip any numbers that are initially the same as current index
+        - make sure to pop to undo (as we are skipping the number)
+    
+'''
