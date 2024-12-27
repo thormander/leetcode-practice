c724710e3ee277bc3bc73a134e2c82efb90cd94b
Thor <71967190+thormander@users.noreply.github.com>
2024-12-27 17:44:50 +1000

    Update Palindrome Partitioning.py

--- Palindrome Partitioning.py
+++ Palindrome Partitioning.py
@@ -1,30 +1,56 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         result = []
-        partition = []
-        
+        temp_p = []
 
-        #backtrack
-        def DFS(i):
-            # base case
-            if i >= len(s):
-                result.append(partition.copy())
+        def backtrack(index):
+            if index >= len(s):
+                result.append(temp_p.copy())
                 return
             
-            for j in range(i,len(s)):
-                if self.isPalindrome(s,i,j):
-                    partition.append(s[i:j+1])
-                    DFS(j+1) # if it is, we can continue searching
-                    partition.pop()
-                else:
-                    continue
-
-        DFS(0)
+            for j in range(index,len(s)):
+                if self.is_palindrome(s[index:j+1]) == True:
+                    temp_p.append(s[index:j+1])
+                    backtrack(j+1)
+                    temp_p.pop()
+        
+        backtrack(0)
         return result
         
-    # helper
-    def isPalindrome(self, s, i, j):
-        s_to_check = s[i:j+1]
-        return s_to_check == s_to_check[::-1]
-        
+    def is_palindrome(self, s):
+        # 2 pointer methdo
+        left = 0
+        right = len(s) - 1
 
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True        
+
+
+
+'''
+helper function returns T/F if its a palindrome
+
+"aab"
+
+a      aa aab
+| \     | 
+a  ab  b
+|
+b
+
+partition tree partition(index):
+    base case, if our index is out of bounds, add that partition
+
+    go through every other character not on index:
+        if index --> j in s is a palindrome:
+            add to results
+            recurse and increment index
+            pop the partition to clean up
+
+'''
