fcdfcfe46e20f437f6701cadc81d1441df702797
Thor <71967190+thormander@users.noreply.github.com>
2025-01-08 12:35:24 -0600

    Update Jump Game.py

--- Jump Game.py
+++ Jump Game.py
@@ -1,16 +1,30 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        endPointer = len(nums) - 1
+        can_reach_end = len(nums) - 1
 
+        # iterate in reverse
         for i in range(len(nums)-1,-1,-1):
-            if i + nums[i] >= endPointer:
-                endPointer = i
+            if i + nums[i] >= can_reach_end:
+                # we know it is reachable
+                can_reach_end = i
         
-        if endPointer == 0:
-            return True
-        else:
-            return False
+        return can_reach_end == 0
 
-        '''
-        approach in reverse; pointer at end, decrement as we step down to see if we can reach it 
-        '''
+'''
+greedy approach
+
+take steps needed to reach the next biggest index
+ - within current range
+
+But if we do in reverse,
+
+[2,3,1,1,4]
+ |
+
+pointer is what we want reach
+
+lets iterate in reverse:
+    can we reach the pointer from our index?
+
+    if i reaches index 0, we know the end is reachable --> return True
+'''
