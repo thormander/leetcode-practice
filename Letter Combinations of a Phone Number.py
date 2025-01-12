8005b1b97aed1fef8533a2cf55de4af302b517f4
Thor <71967190+thormander@users.noreply.github.com>
2025-01-12 15:17:40 -0600

    Update Letter Combinations of a Phone Number.py

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -1,22 +1,42 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        nToL = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        hashmap = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+        }
+
         result = []
 
-        # backtracking
-        def backtrack(i,cur_s):
+        def backtrack(index,cur_s):
             # base case
-            if len(cur_s) >= len(digits):
+            if len(digits) == len(cur_s):
+                # we know we found one
                 result.append(cur_s)
                 return
             
-            for char in nToL[digits[i]]:
-                backtrack(i+1,cur_s + char)
-        
-        if digits: 
-            backtrack(0,"")
-        else:
-            return []
-        
-        return result
+            for char in hashmap[digits[index]]:
+                backtrack(index+1,cur_s + char)
+            
+        backtrack(0,"")
 
+        if digits == "":
+            return []
+        else:
+            return result
+
+
+'''
+"23"
+
+      2
+|      \          \
+a       b          c
+| \ \   | \ \      | \ \
+d  e f  d  e f     d  e f
+'''
