955fe24448a3acb6bc83cace004de0e21fbef942
Thor <71967190+thormander@users.noreply.github.com>
2025-01-21 16:42:34 -0600

    Update 728. Self Dividing Numbers.py

--- 728. Self Dividing Numbers.py
+++ 728. Self Dividing Numbers.py
@@ -2,26 +2,26 @@ class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         result = []
 
-        def self_dividing(num: int) -> bool:
-            string_num = str(num)
-            for char in string_num:
-                if char == '0' or num % int(char) != 0:
+        # help function to determine if self dividing
+        def dividing(number: int) -> bool:
+            for char in str(number):
+                if char == '0' or number % int(char) != 0:
                     return False
-            
             return True
 
-        for i in range(left,right + 1):
-            if self_dividing(i) == True:
-                result.append(i)
+        for number in range(left,right+1):
+            if dividing(number) == True:
+                result.append(number)
         
         return result
-            
-
 
 
 '''
-range between left and right INCLUSIVE
+helper function:
+    what is a self diving nubmer? return true false
+    converting the integer to a string and iterating through it
 
-helper function --> boolean
-    check if its self dividing
+iterate between lef4t and right and check if the number is self dividing
+
 '''
+        
