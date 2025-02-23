fcc88a1ee8159d271ffd94d7609e3de57685ebbb
Thor <71967190+thormander@users.noreply.github.com>
2025-02-23 16:04:45 -0600

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,38 +1,37 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre = 1
-        post = 1
+        prefix = 1
+        postfix = 1
 
         result = [1] * len(nums)
 
-        # 1st step (l --> r)
-        for i in range(len(nums)):
-            result[i] = pre # set
-            pre *= nums[i] # update
-
-        # 2nd step (r --> l)
+        # 1st pass
+        for i,v in enumerate(nums):
+            result[i] *= prefix
+            prefix *= v
+        
+        # 2nd pass
         for i in range(len(nums)-1,-1,-1):
-            result[i] *= post # set
-            post *= nums[i] # update
+            result[i] *= postfix
+            postfix *= nums[i]
         
         return result
 
+
+
+
+
 '''
+[1,1,2,6] 1
+
+1 [1,1,2,6] -- 1st pass
+[24,12,8,6] 12
+
 [1,2,3,4]
 
-pre and post
+prefix, postfix --> start with 1
+1st pass go in order
+2nd pass go in reverse
 
-1st step
-
-pre start at 1(left to right) --> at the index that is the result of multiplying nums to left of that number
-[1,1,2,6]
-
-insert pre to i on the result list, then update it by * the current num we are on
-
-2nd step (go in reverse)
-post start 1
-[24,12,8,6] 
-
-SET then UPDATE
-
-'''
+times prefix to next, then update from nums
+'''  
