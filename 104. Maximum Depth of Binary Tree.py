f525e339764ffdc3891d8efdf4c03f2826106e0c
Thor <71967190+thormander@users.noreply.github.com>
2025-02-25 16:28:04 -0800

    Update 104. Maximum Depth of Binary Tree.py

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -6,39 +6,8 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        layer = 0
-
-        queue = deque()
-
-        if root:
-            queue.append(root)
-
-        while queue:
-            # we want to go layer by layer
-            for i in range(len(queue)):
-                cur_node = queue.popleft()
-
-                if cur_node.left:
-                    queue.append(cur_node.left)
-                if cur_node.right:
-                    queue.append(cur_node.right)
-            
-            layer += 1
-            
-
+        # base case
+        if not root:
+            return 0
         
-        return layer
-
-
-
-'''
-DFS --> dig as far down as possible nad just keep a runnig max
-
-*** BFS --> go layer by layer and return our last layey ***
-
-keep track of layer we are on
-
-do the bfs until we stop and then return the layer we are on
-
-'''
-        
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
