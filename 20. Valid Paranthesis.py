36cfb07c55373b4cb41df556bab0699311a05a37
Thor <71967190+thormander@users.noreply.github.com>
2025-08-13 20:25:16 -0500

    Update 20. Valid Paranthesis.py

--- 20. Valid Paranthesis.py
+++ 20. Valid Paranthesis.py
@@ -1,39 +1,37 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        valid_pars = {"]":"[",")":"(","}":"{"}
         stack = []
 
-        # edge case
-        
+        pairMap = {')':'(','}':'{',']':'['}
 
         for char in s:
-            if not stack and char in valid_pars.keys():
-                return False
-
-            if char in valid_pars.keys() and stack[-1] == valid_pars[char]: # closed 
-                stack.pop()
-            else:
+            if char not in pairMap:
                 stack.append(char)
+            else:
+                if stack and (pairMap[char] == stack[-1]):
+                    stack.pop()
+                else:
+                    return False
         
         if not stack:
             return True
         else:
             return False
-
-
+        
 
 '''
-"...()[]{}"
-         |
+closing is important!
+
+map a closing bracket --> respective opening bracket
+
+use a stack and just add opening brackets to it
+
+anytime we run into a closing, check the top of our stack to see if it matches the closing (if it is pop the top of our stack)
+
+if after everything, our stack still has something, that means its false
+
+Input: s = "([])"
+                |
 []
-
-map where we have pairs of parenthesis
-    - searching on closing, we use closing pars as key
-
-stack ds --> last in first out
- - keep track of our current paraentheis
- - pay attention to closing parenthees
-
-once we hit end, if stack is not empty --> return false because it means at least one par was not closed
 '''
