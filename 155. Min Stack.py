9c15be4bd76e165ba8260514a77086498e0b6566
Thor <71967190+thormander@users.noreply.github.com>
2025-08-16 12:26:43 -0500

    Update 155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -1,45 +1,42 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
         self.minstack = []
-        
+        self.trackMin = []
         
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
+        self.minstack.append(val)
 
-        # min stack logic
-        if not self.minstack:
-            self.minstack.append(val)
+        if not self.trackMin:
+            self.trackMin.append(val)
         else:
-            if val < self.minstack[-1]:
-                self.minstack.append(val)
+            if self.trackMin[-1] > val:
+                self.trackMin.append(val) # append new value if current IS smaller
             else:
-                # keep previous value if not smaller
-                self.minstack.append(self.minstack[-1])
-
+                self.trackMin.append(self.trackMin[-1]) # append previous value if current not smaller
+        
 
     def pop(self) -> None:
-        self.stack.pop()
-
-        # min stack logic
         self.minstack.pop()
+        self.trackMin.pop()
         
 
     def top(self) -> int:
-        return self.stack[-1]
-        
-
-    def getMin(self) -> int:
         return self.minstack[-1]
         
 
-'''
-2 stacks: 1 will be standard that we use 'stack'; 'minstack' also taht we also populate
+    def getMin(self) -> int:
+        return self.trackMin[-1]
 
-1: 1 2 3
-2: 1 1 1
+        
+'''
+[1,2,0,3] -3 end result stack
+           |
+[1,2,0,3] min stack
+[1,1,0,0] min tracking
+
+need a way to track the minimum in our stack, and update as we pop/add
 '''
 
 # Your MinStack object will be instantiated and called as such:
