8b099ff3292368fdd83973745f926b94468398b5
Thor <71967190+thormander@users.noreply.github.com>
2025-08-23 20:27:54 -0500

    Update 739 Daily Temperatures.py

--- 739 Daily Temperatures.py
+++ 739 Daily Temperatures.py
@@ -1,25 +1,31 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = []
+
+        result = [0] * len(temperatures)
+        stack = [] # [[temp,index],....]
 
         for i,v in enumerate(temperatures):
-            
-            while stack and stack[-1][1] < v:
-                temp = stack.pop() # pop [i,v] from top stack
-                # populate position of popped temp with time to wait
-                answer[temp[0]] = i - temp[0]
-
-            stack.append([i,v])
+            # check if greater that top of stack
+            while stack and stack[-1][0] < v:
+                poppedTemp, poppedIndex = stack.pop()
+                result[poppedIndex] = i - poppedIndex
         
-        return answer
+            stack.append([v,i])
+        
+        return result
 
 
 
+        
 
 
 '''
-[1,1,1,0] answer
-[[0,30]] stack
+result[i] == amt of days till it gets warmer, otherwise 0
+
+[30,40,50,30,60]
+              |
+[1,1,2,1,0]
+
+stack = [[60,4]]
+
 '''
-        
