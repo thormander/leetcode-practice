d345a2274fc2833fee7ca0b558b0b423864da6ab
Thor <71967190+thormander@users.noreply.github.com>
2025-08-24 12:41:55 -0500

    Update 704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,28 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         left = 0
         right = len(nums) - 1
 
         while left <= right:
             mid = (right + left) // 2
-
-            # base case
-            if nums[mid] == target:
-                return mid
-            # case when mid is less than target
-            elif nums[mid] < target:
-                left = mid + 1
-            # case when mid is greater than target
-            else:
+            if nums[mid] > target:
+                # need to go left
                 right = mid - 1
-
+            elif nums[mid] < target:
+                # need to go right
+                left = mid + 1
+            else:
+                return mid
+        
         return -1
 
 
 
-
-'''
-binary search
-
-if some target doesnt exist jsut return -1, otherwise return the index it is at.
-'''
