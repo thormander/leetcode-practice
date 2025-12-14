ba5c8a70aa35a97cb4c2b2132324b9bd52cc4deb
Thor <71967190+thormander@users.noreply.github.com>
2025-12-14 17:51:08 -0600

    Update 424. Longest Repeating Character Replacement.py

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -1,44 +1,33 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        result = 0
-        hashmap = {} # key: character | value: occurance
-
+        hashmap = {}
         left = 0
-
-        for right in range(len(s)):
-            # update occurances
+        right = 0
+        result = 0
+        
+        while right < len(s):
+            # handle our hashamp (what chars and how many in window)
             if s[right] not in hashmap:
                 hashmap[s[right]] = 1
-            else: 
+            else:
                 hashmap[s[right]] += 1
             
-            # logic for our left pointer and make sure our window is valid
-            while ((right - left + 1) - max(hashmap.values())) > k:
+            # keep removing chars from left until our window is valid again (within k diff chars)
+            while (right-left+1) - max(hashmap.values()) > k:
                 hashmap[s[left]] -= 1
                 left += 1
-            
-            result = max(result, (right - left + 1))
-        
+
+            result = max(result,sum(hashmap.values()))
+
+            right += 1
+
         return result
 
-         
-
 '''
-2 pointers --> sliding window
+hashmap key: char | value: occurance
+    represents what is in our window
 
-keep track of character counts --> hashmap key:char value: occurances
+running max result
 
-left pointer...
-
-"ABAB"
- L  R
- window size
-(R - L) + 1 - max(hashmap occurances) = 4 - 2 = 2 <= k
-
-A: 2
-B: 2
-
-increment left with while loop until we get within the k again.
-    - also need to make sure we update counts as we do this.
-
+left,  right = 0
 '''
