5ac0e7d7ff7b079778135a4c4e39b3e28bb1e80c
Thor <71967190+thormander@users.noreply.github.com>
2025-12-16 20:11:12 -0600

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -3,21 +3,10 @@ class Solution:
         hashset = set()
 
         for num in nums:
-            # first do check
+            # first check for presence
             if num in hashset:
                 return True
             
             hashset.add(num)
-
-        # if we dont find any
+        
         return False
-    
-'''
-for loop on nums:
-    add each num to a hashset we made
-    as we add, just check if its already in there
-
-    if it is --> return true
-
-return false if we get through the entire for loop
-'''
