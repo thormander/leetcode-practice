c651717bc4a54a5cd2496e6cb2fbc154ddefcd53
Thor <71967190+thormander@users.noreply.github.com>
2025-12-16 21:48:35 -0600

    Refactor checkInclusion method for better readability
    Refactor character counting logic and improve clarity.

--- 567 Permutation in String.py
+++ 567 Permutation in String.py
@@ -1,60 +1,50 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        hashmap_s1 = {}
+        hashmap_s2 = {} # counts of chars in our window
 
-        # s1 reference map
-        mapS1 = {}
+        # s1 (we can permutate)
         for char in s1:
-            if char not in mapS1:
-                mapS1[char] = 1
+            if char in hashmap_s1:
+                hashmap_s1[char] += 1
             else:
-                mapS1[char] += 1
-
-        # s2 check map
-        mapS2 = {}
-        targetSize = len(s1)
-
-
+                hashmap_s1[char] = 1
+        
+        # s2 (where we shift our window)
+        maxWindowSize = len(s1)
         left = 0
+        right = 0
 
-        for right in range(len(s2)):
-            # populate mapS2
-            if s2[right] not in mapS2:
-                mapS2[s2[right]] = 1
+        while right < len(s2):
+            char = s2[right]
+            # fill hashmap s2
+            if char in hashmap_s2:
+                hashmap_s2[char] += 1
             else:
-                mapS2[s2[right]] += 1
-            
-            # shifting left
-            while (right - left + 1) > targetSize:
-                # decrement AND remove if needed (if its 0) -- save on memory
-                if mapS2[s2[left]] > 0:
-                   mapS2[s2[left]] -= 1
+                hashmap_s2[char] = 1
 
-                if mapS2[s2[left]] == 0:
-                    del mapS2[s2[left]]
-
+            # make sure our windo is valid size
+            while (right - left + 1) > maxWindowSize:
+                hashmap_s2[s2[left]] -= 1
+                
+                # handle case if its occurance is now 0
+                if hashmap_s2[s2[left]] == 0:
+                    del hashmap_s2[s2[left]]
                 
                 left += 1
-            
-            if (right - left + 1) == targetSize and mapS1 == mapS2:
-                return True
 
+            if hashmap_s1 == hashmap_s2:
+                return True
+            
+            right += 1
+            
         return False
 
-
-
-
-
-
 '''
-char occurances for both s1 and s2
- - where s1 is our reference
+2 maps - s1 is our reference, traverse through s2
+ key: char | value: occurances
 
-substring that has exact occurances as s1
-    return true
+we know its a substring if we run across a window with same dictionary
 
-first, get our hashmap for s1 and fill it out (reference)
-
-second, go through s2 using pionts
- - shift both if no similarities
- - keep left in position only when we are in s1
+window will be length of s1
 '''
