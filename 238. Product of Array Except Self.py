b76fa57bc54b6ee7f2853485ae55c9e3118421cb
Thor <71967190+thormander@users.noreply.github.com>
2026-01-17 17:35:11 -0600

    Optimize productExceptSelf first pass logic
    Refactor first pass to set result values directly from pre.

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -2,31 +2,32 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         pre = 1
         post = 1
-
         result = [1] * len(nums)
 
         # first pass
-        for i in range(0,len(nums)):
-            result[i] *= pre
+        for i in range(len(nums)):
+            result[i] = pre
             pre *= nums[i]
 
- 
-        # second pass (in reverse)
+        # second pass (reverse)
         for i in range(len(nums)-1,-1,-1):
             result[i] *= post
             post *= nums[i]
 
         return result
 
-
 '''
-pre             post
-24    [1,2,3,4]   24
-      [24,12,8,6]
-    
-first pass
-    - setting that index = to what pre is , then update pre after
+1 [1,2,4,6]
+     |
 
-second
-    - update result with index val * post , update post after
+[1,1,2,8]
+
+hold = 48
+
+now in reverse
+
+hold = 24
+
+[48,24,12,8]
+    |
 '''
